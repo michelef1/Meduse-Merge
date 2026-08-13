ebcfeb6bb6a9ad1249adb01928d8afcf4c9f2870
michelef1 <michelef654321@gmail.com>
2026-08-14 01:05:59 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Incrementa CACHE_NAME ad ogni nuovo deploy per evitare contenuti obsoleti su Android
-const CACHE_NAME = 'meduse-merge-v9';
+const CACHE_NAME = 'meduse-merge-v10';
 const ASSETS = [
   './',
   './index.html',
